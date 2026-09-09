d62be00a6226092c5491a5d1e4c59f9089cacc64
Yash Dubey <40451580+YashDubeyOfficial@users.noreply.github.com>
2026-09-09 15:21:30 +0530

    feat: update GitHub Actions workflow for Next.js deployment and adjust project configuration

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,14 @@
 import type { NextConfig } from "next";
 
+const repoName = "UI-design-inspirations";
+const isProduction = process.env.NODE_ENV === "production";
+const basePath = isProduction ? `/${repoName}` : "";
+
 const nextConfig: NextConfig = {
-  /* config options here */
-  output:"export",
+  output: "export",
+  trailingSlash: true,
+  basePath,
+  assetPrefix: basePath ? `${basePath}/` : undefined,
 };
 
 export default nextConfig;
